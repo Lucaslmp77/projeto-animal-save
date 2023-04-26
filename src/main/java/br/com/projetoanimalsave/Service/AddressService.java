75889a534ffcda708053cf88404451d1547e0156
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-04-26 12:58:00 -0300

    #5 obs: commit feito erradamente na branch feat-address-service.

--- src/main/java/br/com/projetoanimalsave/Service/AddressService.java
+++ src/main/java/br/com/projetoanimalsave/Service/AddressService.java
@@ -6,7 +6,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.ClientInfoStatus;
 import java.util.List;
 
 @Service
