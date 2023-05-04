db21035a907fe852be582eaa0229c6dab201cfd4
Allyson <jandreyallyson@gmail.com>
2023-05-04 17:13:28 -0300

    Feat - Método de cadastrar vacinação.

--- src/main/java/br/com/projetoanimalsave/Controller/VaccinationController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/VaccinationController.java
@@ -1,4 +1,30 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Vaccination;
+import br.com.projetoanimalsave.Service.VaccinationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vaccination")
 public class VaccinationController {
+
+    @Autowired
+    private VaccinationService vaccinationService;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody Vaccination vaccination
+    ) {
+        try {
+            this.vaccinationService.save(vaccination);
+            return ResponseEntity.ok().body("Vacinação cadastrado com sucesso!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
