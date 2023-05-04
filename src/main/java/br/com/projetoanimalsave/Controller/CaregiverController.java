d02317639b119d5daeb379f0828869b648c90af8
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-04 16:29:01 -0300

    post save Caregiver

--- src/main/java/br/com/projetoanimalsave/Controller/CaregiverController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/CaregiverController.java
@@ -1,7 +1,11 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Caregiver;
 import br.com.projetoanimalsave.Service.CaregiverService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,15 @@ public class CaregiverController {
     @Autowired
     private CaregiverService caregiverService;
 
-
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody Caregiver caregiver
+    ) {
+        try {
+            this.caregiverService.save(caregiver);
+            return ResponseEntity.ok().body("Cuidadora cadastrada");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
