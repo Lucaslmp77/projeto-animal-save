c3289a75f002c010f57d020f9275de657e64af86
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-04 15:05:48 -0300

    Post admin

--- src/main/java/br/com/projetoanimalsave/Controller/AdminController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/AdminController.java
@@ -1,4 +1,32 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Admin;
+import br.com.projetoanimalsave.Service.AdminService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+        @RequestBody Admin admin
+    ) {
+        try{
+            this.adminService.save(admin);
+            return ResponseEntity.ok().body("Administrador cadastrado");
+        } catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
