a6dfb595845d842cc856cd0f971816831735193b
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-04 16:26:19 -0300

    Iniciando Caregiver controller

--- src/main/java/br/com/projetoanimalsave/Controller/CaregiverController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/CaregiverController.java
@@ -1,4 +1,16 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Service.CaregiverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/caregiver")
 public class CaregiverController {
+
+    @Autowired
+    private CaregiverService caregiverService;
+
+
 }
