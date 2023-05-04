0f9b7ce6dd0ac38e19b63f7533b5eecbfc2132e3
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-04 16:09:12 -0300

    Post Animal

--- src/main/java/br/com/projetoanimalsave/Controller/AnimalController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/AnimalController.java
@@ -1,4 +1,29 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Animal;
+import br.com.projetoanimalsave.Service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/animal")
 public class AnimalController {
+
+    @Autowired
+    private AnimalService animalService;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody Animal animal
+    ) {
+        try {
+            this.animalService.save(animal);
+            return ResponseEntity.ok().body("Animal cadastrado");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
