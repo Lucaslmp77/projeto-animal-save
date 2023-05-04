11d3a1eafaac3335decd1c6be3016d9511269b4d
Allyson <jandreyallyson@gmail.com>
2023-05-04 16:16:31 -0300

    Feat - Método de cadastrar associado.

--- src/main/java/br/com/projetoanimalsave/Controller/AssociateController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/AssociateController.java
@@ -1,4 +1,30 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Associate;
+import br.com.projetoanimalsave.Service.AssociateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/associate")
 public class AssociateController {
+
+    @Autowired
+    private AssociateService associateService;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody Associate associate
+    ) {
+        try {
+            this.associateService.save(associate);
+            return ResponseEntity.ok().body("Associado cadastrado com sucesso!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
