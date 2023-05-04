da2b87d0c0bd74cdafeba0b6ab64ddcf66792c5e
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-05-04 14:03:13 -0300

    #21 Método save.

--- src/main/java/br/com/projetoanimalsave/Controller/AddressController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/AddressController.java
@@ -1,4 +1,29 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Address;
+import br.com.projetoanimalsave.Service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/address")
 public class AddressController {
+    @Autowired
+    private AddressService addressService;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody Address address
+    ) {
+        try {
+            this.addressService.save(address);
+            return ResponseEntity.ok().body("Endereço cadastrado!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
