d1d060d14b87b2551428971128e105bbe36a0ec0
Allyson <jandreyallyson@gmail.com>
2023-05-04 17:01:29 -0300

    Feat - Método de cadastrar fornecedores.

--- src/main/java/br/com/projetoanimalsave/Controller/ProviderController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/ProviderController.java
@@ -1,4 +1,30 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Provider;
+import br.com.projetoanimalsave.Service.ProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/provider")
 public class ProviderController {
+
+    @Autowired
+    private ProviderService providerService;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody Provider provider
+    ) {
+        try {
+            this.providerService.save(provider);
+            return ResponseEntity.ok().body("Fornecedor cadastrado com sucesso!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
