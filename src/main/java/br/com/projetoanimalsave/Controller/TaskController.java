49482cc5132d543094ae3df6627030e700593f0c
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-04 17:07:24 -0300

    Método Salvar - Task

--- src/main/java/br/com/projetoanimalsave/Controller/TaskController.java
+++ src/main/java/br/com/projetoanimalsave/Controller/TaskController.java
@@ -1,4 +1,30 @@
 package br.com.projetoanimalsave.Controller;
 
+import br.com.projetoanimalsave.Entity.Task;
+import br.com.projetoanimalsave.Service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/task")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping()
+    public ResponseEntity<?> save(
+            @RequestBody Task task
+    ) {
+        try {
+            this.taskService.save(task);
+            return ResponseEntity.ok().body("Tarefa cadastrada");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
