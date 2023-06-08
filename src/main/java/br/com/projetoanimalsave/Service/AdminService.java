2928dc64b8cd05e7fba42b4d510cd127520964cb
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-06-08 18:46:12 -0300

    Removendo dependências do oauth2 e configuração no aplication properties.

--- src/main/java/br/com/projetoanimalsave/Service/AdminService.java
+++ src/main/java/br/com/projetoanimalsave/Service/AdminService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AdminService implements UserDetailsService {
+public class AdminService implements UserDetailsService{
     @Autowired
     private AdminRepository adminRepository;
 
