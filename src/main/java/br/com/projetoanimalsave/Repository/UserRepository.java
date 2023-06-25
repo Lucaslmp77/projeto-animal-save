667b19572cf9b5b4c0af53531eeb4e6d4a65b7ea
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-06-25 20:24:16 -0300

    Método de selecionar um associado por id de usuário.

--- src/main/java/br/com/projetoanimalsave/Repository/UserRepository.java
+++ src/main/java/br/com/projetoanimalsave/Repository/UserRepository.java
@@ -1,11 +1,13 @@
 package br.com.projetoanimalsave.Repository;
 
-import br.com.projetoanimalsave.Dto.NewPassword;
+import br.com.projetoanimalsave.Entity.Associate;
 import br.com.projetoanimalsave.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,4 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query("UPDATE User user SET user.password = :newPassword WHERE user.id = :id")
     public void newPassword(String newPassword, Long id);
+
+    @Query("SELECT associate FROM Associate associate" +
+            " INNER JOIN associate.user user WHERE user.id = :id")
+    public Optional<Associate> findAssociateByIdUser(Long id);
+
 }
