285784efe82745d1339098fc44a637b0d2a52956
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-06-27 14:45:36 -0300

    Adicionado metodo de listar serviços por id de fornecedor.

--- src/main/java/br/com/projetoanimalsave/Repository/TaskRepository.java
+++ src/main/java/br/com/projetoanimalsave/Repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package br.com.projetoanimalsave.Repository;
 
+import br.com.projetoanimalsave.Entity.Provider;
 import br.com.projetoanimalsave.Entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -7,9 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     @Modifying
     @Query("UPDATE Task task SET task.active = false WHERE task.id = :id")
     public void disable(@Param("id") Long id);
+
+    @Query("SELECT task FROM Task task where task.provider.id = :id")
+    public List<Task> findTaskByIdProvider(Long id);
 }
