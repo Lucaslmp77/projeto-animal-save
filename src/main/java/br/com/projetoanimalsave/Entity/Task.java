75a7de32aebc52edacb1ba212b433248f0f589e5
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-06-30 21:14:39 -0300

    Testes unitários dos métodos set da classe Task.

--- src/main/java/br/com/projetoanimalsave/Entity/Task.java
+++ src/main/java/br/com/projetoanimalsave/Entity/Task.java
@@ -8,22 +8,18 @@ import lombok.Setter;
 @Table(name = "tb_serviços", schema = "projeto-animal-save")
 public class Task extends AbstractEntity {
     @Getter
-    @Setter
     @Column(name = "nome", length = 50, nullable = false)
     private String name;
 
     @Getter
-    @Setter
     @Column(name = "custo", length = 50, nullable = false)
     private Integer cost;
 
     @Getter
-    @Setter
     @Column(name = "valor-mensal", length = 50, nullable = false)
     private Integer monthlyAmount;
 
     @Getter
-    @Setter
     @Column(name = "descrição", length = 200, nullable = false)
     private String description;
 
@@ -32,4 +28,64 @@ public class Task extends AbstractEntity {
     @Setter
     @JoinColumn(name="provider_id", nullable = false)
     private Provider provider;
+
+    public void setName(String name) {
+        if(name == null) {
+            throw new RuntimeException("O nome do serviço inserido é nulo");
+        } else if (name.isEmpty()) {
+            throw new RuntimeException("O nome do serviço inserido está vazio");
+        } else if (name.trim().length() < 3) {
+            throw new RuntimeException("O nome do serviço inserido é muito curto");
+        } else if (name.trim().length() > 30) {
+            throw new RuntimeException("O nome do serviço inserido ultrapassa o limite máximo");
+        } else if (name.matches("[0-9]+")) {
+            throw new RuntimeException("O nome do serviço inserido é composto por números");
+        }
+        else {
+            this.name = name;
+        }
+    }
+
+    public void setCost(Integer cost) {
+        if(cost == null) {
+            throw new RuntimeException("O custo inserido tem o valor nulo");
+        } else if (cost < 0) {
+            throw new RuntimeException("O custo inserido é menor que 0");
+        } else if (cost > 1000000000) {
+            throw new RuntimeException("O custo inserido ultrapassa o limite máximo de caracteres");
+        }
+        else {
+            this.cost = cost;
+        }
+    }
+
+    public void setMonthlyAmount(Integer monthlyAmount) {
+        if(monthlyAmount == null) {
+            throw new RuntimeException("A quantidade mensal inserida tem o valor nulo");
+        } else if (monthlyAmount < 0) {
+            throw new RuntimeException("A quantidade mensal inserida é menor que 0");
+        } else if (monthlyAmount > 1000000000) {
+            throw new RuntimeException("A quantidade mensal inserida o limite máximo de caracteres");
+        }
+        else {
+            this.monthlyAmount = monthlyAmount;
+        }
+    }
+
+    public void setDescription(String description) {
+        if(description == null) {
+            throw new RuntimeException("A descrição registrada no serviço é nulo");
+        } else if (description.isEmpty()) {
+            throw new RuntimeException("A descrição registrada no serviço está vazio");
+        } else if (description.trim().length() < 3) {
+            throw new RuntimeException("A descrição registrada no serviço é muito curto");
+        } else if (description.trim().length() > 200) {
+            throw new RuntimeException("A descrição registrada no serviço ultrapassa o limite máximo");
+        } else if (description.matches("[0-9]+")) {
+            throw new RuntimeException("A descrição registrada no serviço é composto por números");
+        }
+        else {
+            this.description = description;
+        }
+    }
 }
